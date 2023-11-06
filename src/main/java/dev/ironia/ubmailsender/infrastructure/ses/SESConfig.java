f8396fb789b75f7290d4f3ea539ffd8c594d5802
r41l1m4 <rai.lima17@gmail.com>
2023-11-06 12:05:36 -0300

    add Amazon SES config

---
+++ src/main/java/dev/ironia/ubmailsender/infrastructure/ses/SESConfig.java
@@ -0,0 +1,17 @@
+package dev.ironia.ubmailsender.infrastructure.ses;
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SESConfig {
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService() {
+        return AmazonSimpleEmailServiceClientBuilder
+                .standard()
+                .build();
+    }
+}
