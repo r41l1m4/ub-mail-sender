6a6e0bfa8f9995488ab711c0df3e8512532dde79
r41l1m4 <rai.lima17@gmail.com>
2023-11-06 09:41:34 -0300

    Initial commit

---
+++ src/main/java/dev/ironia/ubmailsender/UbMailSenderApplication.java
@@ -0,0 +1,13 @@
+package dev.ironia.ubmailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbMailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbMailSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ironia/ubmailsender/UbMailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ironia.ubmailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbMailSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
